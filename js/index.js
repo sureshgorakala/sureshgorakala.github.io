8358265e58234d4b2a12a2ada3ee17d3d31d0399
Suresh Gorakala <sureshkumargorakala@gmail.com>
2017-09-20 17:53:40 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -34,20 +34,39 @@ function chatbotResponse() {
     botMessage = 'My name is ' + botName;
   }
 */
-  var request = new XMLHttpRequest();
-request.onreadystatechange = function() {
-    if (request.readyState === 4) {
-        if (request.status === 200) {
-            document.body.className = 'ok';
-            console.log(request.responseText);
-        } else {
-            document.body.className = 'error';
-        }
-    }
-};
-request.open("GET", url , true);
-request.send(null);
-botMessage = request.responseText;
+		var data_file = "https://sureshgorakala.pythonanywhere.com/get/how are you?";
+            var http_request = new XMLHttpRequest();
+            try{
+               // Opera 8.0+, Firefox, Chrome, Safari
+               http_request = new XMLHttpRequest();
+            }catch (e){
+               // Internet Explorer Browsers
+               try{
+                  http_request = new ActiveXObject("Msxml2.XMLHTTP");
+					
+               }catch (e) {
+				
+                  try{
+                     http_request = new ActiveXObject("Microsoft.XMLHTTP");
+                  }catch (e){
+                     // Something went wrong
+         //            alert("Your browser broke!");
+                     return false;
+                  }
+					
+               }
+            }
+			
+            http_request.onreadystatechange = function(){
+               if (http_request.readyState == 4 ){
+                  // Javascript function JSON.parse to parse JSON data
+                  botMessage = http_request.responseText;
+		 
+               }
+            }
+			
+            http_request.open("GET", data_file, true);
+            http_request.send();
 
 
 
