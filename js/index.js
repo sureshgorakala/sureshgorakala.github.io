4e9d5ad33705fb6aba381f8eaf6bf89ce16d521d
Suresh Gorakala <sureshkumargorakala@gmail.com>
2017-09-20 15:40:28 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -33,30 +33,7 @@ function chatbotResponse() {
   if (lastUserMessage === 'name') {
     botMessage = 'My name is ' + botName;
   }
-  alert(botMessage)
-   /*
-  var HttpClient = function() {
-    this.get = function(aUrl, aCallback) {
-        var anHttpRequest = new XMLHttpRequest();
-        anHttpRequest.onreadystatechange = function() { 
-            if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
-                aCallback(anHttpRequest.responseText);
-        }
-
-        anHttpRequest.open( "GET", aUrl, true );            
-        anHttpRequest.send( null );
-    }
-}
-
-  
-  
-  var client = new HttpClient();
-client.get("http://localhost:5000/get/"+lastUserMessage , function(response) {
-    botMessage  = response
-}); */
-
-  
-  
+   
 }
 //****************************************************************
 //****************************************************************
